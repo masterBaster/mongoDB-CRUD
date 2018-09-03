60ef2574a4b8bdccb854bf9dc6940d1fb04d4e6d
masterBaster <bartek.baster@gmail.com>
2018-09-03 10:01:03 +0200

    intro to mocha

---
+++ test/demo_test.js
@@ -0,0 +1,10 @@
+let assert = require('assert');
+
+//describe test
+describe('some demo test', function(){
+    //create tests
+    it('add two numbers together', function(){
+        assert(2 + 4 === 6);
+    });
+});
+
