588a33f340c043636032717e4a6c90224575b20b
masterBaster <bartek.baster@gmail.com>
2018-09-04 11:56:54 +0200

    found a record by object ID

--- test/finding_test.js
+++ test/finding_test.js
@@ -3,8 +3,10 @@ let MarioChar = require('../models/mariochar')
 
 describe('finding records', function(){
 
+    let char;
+
     beforeEach(function(done){
-        let char = new MarioChar({
+        char = new MarioChar({
             name: 'Mario',
             weight: 89,
             superStrenght: true});
@@ -19,7 +21,14 @@ describe('finding records', function(){
         MarioChar.findOne({name: 'Mario'}).then(function(result){
             assert(result.name === 'Mario');
             done();
-        })
+        });
+    });
+
+    it('find one record by ID from the database', function(done){
+        MarioChar.findOne({_id: char._id}).then(function(result){
+            assert(result._id.toString() === char._id.toString());
+            done();
+        });
     });
 });
 
