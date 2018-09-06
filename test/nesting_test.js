24346637e77f4b2dc39ff04de72fd8972f8e8bdf
masterBaster <bartek.baster@gmail.com>
2018-09-06 13:11:38 +0200

    added new book to our sub-dok works perfectly but our db doesn t drop existing record

--- test/nesting_test.js
+++ test/nesting_test.js
@@ -5,10 +5,11 @@ let Author = require('../models/author');
 //describe our test
 describe('nesting records', function(){
 
-    let pat;
+
+
     
     it('creates an authors with sub-dokuments', function(done){
-        pat = new Author({
+        let pat = new Author({
             name: 'Patrick Rothfuss',
             books: [{title: 'Name of the Wind', pages: 400}]
         });
@@ -19,5 +20,25 @@ describe('nesting records', function(){
                 done();
             })
         });
-    })
-})
+    });
+
+    it('adds a book to the author', function(done){
+        let pat = new Author({
+            name: 'Patrick Rothfuss',
+            books: [{title: 'Name of the Wind', pages: 400}]
+        });
+
+        pat.save().then(function(){
+            Author.findOne({name:'Patrick Rothfuss'}).then(function(result){
+                //add a book to the books collections
+                result.books.push({title: "Wise Man's Fear", pages: 500});
+                result.save().then(function(){
+                    Author.findOne({name: 'Patrick Rothfuss'}).then(function(record){
+                        assert(record.books.length === 2);
+                        done();
+                    });
+                });
+            });
+        });
+    });
+});
